8f6f51e0e3f5c09a0b9ae40792cf7a16a99eaff4
vadimsusername <vadimburyakovsky@yahoo.com>
2020-04-09 18:55:07 -0400

    added boiler plate Contact component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import About from "./components/About";
-
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello From App.js</h1>
       <About></About>
-      
+      <Portfolio></Portfolio>
+      <Contact></Contact>
     </div>
   );
 }
---
+++ src/components/Contact/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Contact() {
+
+    return (
+        <>
+            <h4>Hello From Contact Component</h4>
+        </>
+    );
+}
+
+export default Contact;
