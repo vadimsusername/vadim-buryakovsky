49030ee2330c4a51df55978f08fefe654eea8c21
vadimsusername <vadimburyakovsky@yahoo.com>
2020-04-09 18:17:43 -0400

    added boiler plate About page component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import About from "./components/About";
 function App() {
   return (
     <div className="App">
-      <h1>Hello From App.js, Vadim </h1>
+      <h1>Hello From App.js</h1>
+      <About />
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function About() {
+
+    return (
+        <>
+            <h2>Hello From About Component</h2>
+        </>
+    );
+}
+
+export default About;
