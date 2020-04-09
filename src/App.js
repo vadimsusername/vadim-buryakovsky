bc2bd3e68ce8c446119961169c57977222c0f76b
vadimsusername <vadimburyakovsky@yahoo.com>
2020-04-09 18:35:43 -0400

    added boiler plate Portfolio component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import About from "./components/About";
+
+
 function App() {
   return (
     <div className="App">
       <h1>Hello From App.js</h1>
-      <About />
+      <About></About>
+      
     </div>
   );
 }
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Portfolio() {
+
+    return (
+        <>
+            <h3>Hello From Portfolio Component</h3>
+        </>
+    );
+}
+
+export default Portfolio;
