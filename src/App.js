bff8476b4a0b41790aa6cd8179e49bdcbbf207f3
vadimsusername <vadimburyakovsky@yahoo.com>
2020-04-09 19:05:42 -0400

    added boiler plate Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 
+
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <h1>Hello From App.js</h1>
       <About></About>
       <Portfolio></Portfolio>
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+
+    return (
+        <>
+            <h5>Hello From Header Component</h5>
+        </>
+    );
+}
+
+export default Header;
