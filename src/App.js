c3eb915dea734117162fc101c04417a59bd71696
vadimsusername <vadimburyakovsky@yahoo.com>
2020-04-09 20:30:40 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
-
+import Four04 from "./components/Four04";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <h1>Hello From App.js</h1>
-      <About></About>
-      <Portfolio></Portfolio>
-      <Contact></Contact>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={About}></Route>
+          <Route exact path="/about" component={About}></Route>
+          <Route exact path="/portfolio" component={Portfolio}></Route>
+          <Route exact path="/contact" component={Contact}></Route>
+          <Route component={Four04}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Four04/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Four04() {
+
+    return (
+        <>
+            <h2>404 Page Does Not Exist</h2>
+        </>
+    );
+}
+
+export default Four04;
